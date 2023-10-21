dc729170049b256411e8a33776db7bcbdd97de3f
Gehad799 <gehadalaa799@gmail.com>
2023-10-21 14:42:00 +0300

    projectfiles

---
+++ src/useMovies.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+const KEY = "f6105a65";
+
+export function useMovies(query) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(
+    function () {
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+          );
+
+          if (!res.ok)
+            throw new Error("Something went wrong with fetching movies.");
+
+          const data = await res.json();
+          if (data.Response === "False") throw new Error("Movie not found.");
+          setMovies(data.Search);
+          setError("");
+        } catch (err) {
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
+        } finally {
+          setIsLoading(false);
+        }
+      }
+      // we have at least to write 3 char to start displaying the query
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+      fetchMovies();
+    },
+    [query]
+  );
+  return { movies, error, isLoading };
+}
